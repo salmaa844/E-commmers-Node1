a0bf28b01208cd7badbbf371d23f9ddadddd8898
salma ali <alisalma455@gmail.com>
2025-08-13 13:33:36 +0300

    validation for cart

--- src/modules/cart/cart.router.js
+++ src/modules/cart/cart.router.js
@@ -3,15 +3,19 @@ import asyncHandler from "../../utils/asyncHandler.js";
 import * as controller from "./cart.controller.js"
 import authenticateJWT from "./../../middlewares/authMiddleware.js"
 import ROLE from "../../../Database/roles.js";
+import { validate } from "../../middlewares/validation.js";
+import * as Schemas from "../../validation/cart.validator.js"
 
 const router = Router();
 
 router.post("/add",
     authenticateJWT([ROLE.USER]),
+    validate(Schemas.addToCart),
     asyncHandler(controller.addToCartController)
 );
 router.patch("/update-quantity",
       authenticateJWT([ROLE.USER]),
+      validate(Schemas.updateQunt),
     asyncHandler(controller.updateQuntity));
 router.delete("/remove",
        authenticateJWT([ROLE.USER]),
--- src/validation/cart.validator.js
+++ src/validation/cart.validator.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
-import objectId from "./objectId.validator";
+import objectId from "./objectId.validator.js";
 export const updateQunt = Joi.object({
     productId:objectId.required(),
     qnt:Joi.number().min(1).required()
@@ -10,9 +10,10 @@ export const addToCart= Joi.object({
      productId:objectId.required(),
      attributes:Joi.array().items(
         Joi.object({
-            name: Joi.string().required,
-            value: Joi.alternatives().try(Joi.string(),Joi.number()).required()
-
+            name: Joi.string().required(),
+            value: Joi.array().
+            items(Joi.alternatives().try(Joi.string(),Joi.number()).required()
+        ).required()
         })
      ).optional()
 })
