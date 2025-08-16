6be19b1886ddc2d70bfdfc9708ed5f925a51553a
Salma Ghafri <138569162+salmaa844@users.noreply.github.com>
2025-08-16 14:00:46 +0300

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -25,7 +25,7 @@ const init = (express, app) => {
     app.use(express.json());
 
     app.use(cors({
-        origin:"http://localhost:5173",
+        origin:"*",
         credentials:true
     }))
     app.get("/",(req,res)=>{
